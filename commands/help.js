c1c5c9160f43e5d3ff1f36305b8b71c4ce906939
Dan Poage <daniel.poage@auctane.com>
2025-07-11 22:06:48 -0600

    3.9.14: Added cards and quest for ALEP14: Fangs in the Dark

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 module.exports = function help(channel) {
   channel.send(
-    "**BeornBot** - Lord of the Rings: The Card Game - Bear Bot 3.9.13 (2025-02-23)\n\n" +
+    "**BeornBot** - Lord of the Rings: The Card Game - Bear Bot 3.9.14 (2025-07-11)\n\n" +
       "List of Commands:\n" +
       "!help - This help message\n" +
       "!hob <query> - Find and display card text from Hall of Beorn\n" +
